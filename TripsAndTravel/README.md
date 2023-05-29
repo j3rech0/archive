5d70c0eaac82a5d985c8ee11919120c98b12488a
Jech <j3re.ch0@gmail.com>
2023-05-30 00:59:39 +0800

    trips and travel

---
+++ TripsAndTravel/README.md
@@ -0,0 +1,1 @@
+![Trips And Travel](./screenshot.png)
---
+++ TripsAndTravel/js/all.js
@@ -0,0 +1,7 @@
+//techforgeonline.com - jech
+//jquery
+document.write('<script type="text/javascript" src="thirdparty/slider/js/jquery.js"></script>');
+//slider home
+document.write('<script type="text/javascript" src="thirdparty/slider/js/scripts.js"></script>');
+//popup login
+document.write('<script type="text/javascript" src="thirdparty/popupjquery/popup.js"></script>');
